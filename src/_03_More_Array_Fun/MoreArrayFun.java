8af753021662afbbd9dbd57ab792f8036ca72867
League Student <league@iMac-11.attlocal.net>
2019-10-14 18:28:08 -0700

    ArrayMethods, Firework, MoreArrayFun (IP)

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,58 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+		
+		String[] strings = {"Waffle","Pizza","Josh","Jackie","Saw","Snow","Tundra","Cabin"};
+		
+		printStrings(strings);
+		reverseStrings(strings);
+		everyOther(strings);
+		randomStrings(strings);
+		
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	public static void printStrings(String[] strings) {
+		for (int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	
+	public static void reverseStrings(String[] strings) {
+		for (int i = strings.length; i < 0; i--) {
+			System.out.println(strings[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	public static void everyOther(String[] strings) {
+		for (int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i%2]);
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	public static void randomStrings(String[] array) {
+		Random rand = new Random();
+		boolean[] printed = new boolean[array.length];
+		int amount = array.length;
+		
+		while (amount > 0) {
+			int index = rand.nextInt(array.length);
+			if(!printed[index]) {
+				System.out.println(array[index]);
+				printed[index] = true;
+				amount--;
+			}
+		}
+	}
 	
 }
---
+++ src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+	public static void main(String[] args) {
+		
+		System.out.println(1/2);
+		System.out.println(1/4);
+		System.out.println(1/3);
+		System.out.println(Integer.MIN_VALUE);
+		
+	}
+}
